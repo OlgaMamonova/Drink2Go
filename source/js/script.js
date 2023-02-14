6669e11e253f5dbeaaa968e53f4d26929fe39643
Olga Mamonova <shamrilo91@mail.ru>
2023-02-15 00:56:46 +0300

    добавляет стили ралио и чекбокс

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,24 @@
-import { initBurger } from "./burger.js";
+const initBurger = () => {
+  const burgerToggle = document.querySelector('.header__burger-toggle');
+  const headerNav = document.querySelector('.header__nav');
+
+
+  const openMenu = () => {
+    burgerToggle.classList.add('header__burger-toggle--menu-opened');
+    headerNav.classList.add('header__nav--menu-opened');
+
+    let burgerCloseButton = document.querySelector('.header__burger-toggle--menu-opened');
+    burgerCloseButton.addEventListener('click', closeMenu);
+  }
+
+  function closeMenu () {
+    burgerToggle.classList.remove('header__burger-toggle--menu-opened');
+    headerNav.classList.remove('header__nav--menu-opened');
+    burgerCloseButton.removeEventListener('click', closeMenu);
+  }
+
+  burgerToggle.addEventListener('click', openMenu);
+}
+
 
 initBurger();
