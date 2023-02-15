cc3b2d96a58c45951c74064ad1520e7f51e8483b
Olga Mamonova <shamrilo91@mail.ru>
2023-02-15 23:29:40 +0300

    добавляет сетку каталога

--- source/js/script.js
+++ source/js/script.js
@@ -1,24 +1,3 @@
-const initBurger = () => {
-  const burgerToggle = document.querySelector('.header__burger-toggle');
-  const headerNav = document.querySelector('.header__nav');
-
-
-  const openMenu = () => {
-    burgerToggle.classList.add('header__burger-toggle--menu-opened');
-    headerNav.classList.add('header__nav--menu-opened');
-
-    let burgerCloseButton = document.querySelector('.header__burger-toggle--menu-opened');
-    burgerCloseButton.addEventListener('click', closeMenu);
-  }
-
-  function closeMenu () {
-    burgerToggle.classList.remove('header__burger-toggle--menu-opened');
-    headerNav.classList.remove('header__nav--menu-opened');
-    burgerCloseButton.removeEventListener('click', closeMenu);
-  }
-
-  burgerToggle.addEventListener('click', openMenu);
-}
-
+import { initBurger } from "./burger.js";
 
 initBurger();
